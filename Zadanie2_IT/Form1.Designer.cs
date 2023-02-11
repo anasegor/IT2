96ccbf3b686c957816a169f416b63c7f88131564
anasegor <a_n_a_s@inbox.ru>
2023-02-12 00:45:34 +0300

    gir add

---
+++ Zadanie2_IT/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿
+namespace Zadanie2_IT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.for_N2 = new System.Windows.Forms.TextBox();
+            this.for_v2 = new System.Windows.Forms.TextBox();
+            this.for_v3 = new System.Windows.Forms.TextBox();
+            this.for_N = new System.Windows.Forms.TextBox();
+            this.for_fd = new System.Windows.Forms.TextBox();
+            this.for_N1 = new System.Windows.Forms.TextBox();
+            this.for_v1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.for_N1_2 = new System.Windows.Forms.TextBox();
+            this.for_N2_2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.for_sill = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.for_T = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(4, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(484, 191);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(4, 232);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(485, 206);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.for_T);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.for_sill);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.for_N2);
+            this.groupBox1.Controls.Add(this.for_v2);
+            this.groupBox1.Controls.Add(this.for_v3);
+            this.groupBox1.Controls.Add(this.for_N);
+            this.groupBox1.Controls.Add(this.for_fd);
+            this.groupBox1.Controls.Add(this.for_N1);
+            this.groupBox1.Controls.Add(this.for_v1);
+            this.groupBox1.Location = new System.Drawing.Point(494, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 138);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Параметры";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(220, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "N2=";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(220, 38);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "N1=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(121, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "N=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(114, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "f_d=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "v2=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "v3=";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "v1=";
+            // 
+            // for_N2
+            // 
+            this.for_N2.Location = new System.Drawing.Point(245, 57);
+            this.for_N2.Name = "for_N2";
+            this.for_N2.Size = new System.Drawing.Size(49, 20);
+            this.for_N2.TabIndex = 6;
+            // 
+            // for_v2
+            // 
+            this.for_v2.Location = new System.Drawing.Point(42, 45);
+            this.for_v2.Name = "for_v2";
+            this.for_v2.Size = new System.Drawing.Size(49, 20);
+            this.for_v2.TabIndex = 5;
+            // 
+            // for_v3
+            // 
+            this.for_v3.Location = new System.Drawing.Point(42, 75);
+            this.for_v3.Name = "for_v3";
+            this.for_v3.Size = new System.Drawing.Size(49, 20);
+            this.for_v3.TabIndex = 4;
+            // 
+            // for_N
+            // 
+            this.for_N.Location = new System.Drawing.Point(146, 31);
+            this.for_N.Name = "for_N";
+            this.for_N.Size = new System.Drawing.Size(49, 20);
+            this.for_N.TabIndex = 3;
+            // 
+            // for_fd
+            // 
+            this.for_fd.Location = new System.Drawing.Point(146, 57);
+            this.for_fd.Name = "for_fd";
+            this.for_fd.Size = new System.Drawing.Size(49, 20);
+            this.for_fd.TabIndex = 2;
+            // 
+            // for_N1
+            // 
+            this.for_N1.Location = new System.Drawing.Point(245, 31);
+            this.for_N1.Name = "for_N1";
+            this.for_N1.Size = new System.Drawing.Size(49, 20);
+            this.for_N1.TabIndex = 1;
+            // 
+            // for_v1
+            // 
+            this.for_v1.Location = new System.Drawing.Point(42, 19);
+            this.for_v1.Name = "for_v1";
+            this.for_v1.Size = new System.Drawing.Size(49, 20);
+            this.for_v1.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(591, 245);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "N2*=";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(591, 219);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "N1*=";
+            // 
+            // for_N1_2
+            // 
+            this.for_N1_2.Location = new System.Drawing.Point(628, 216);
+            this.for_N1_2.Name = "for_N1_2";
+            this.for_N1_2.Size = new System.Drawing.Size(49, 20);
+            this.for_N1_2.TabIndex = 13;
+            // 
+            // for_N2_2
+            // 
+            this.for_N2_2.Location = new System.Drawing.Point(628, 245);
+            this.for_N2_2.Name = "for_N2_2";
+            this.for_N2_2.Size = new System.Drawing.Size(49, 20);
+            this.for_N2_2.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(681, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(479, 104);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(10, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "t";
+            // 
+            // for_sill
+            // 
+            this.for_sill.Location = new System.Drawing.Point(134, 109);
+            this.for_sill.Name = "for_sill";
+            this.for_sill.Size = new System.Drawing.Size(46, 20);
+            this.for_sill.TabIndex = 13;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(85, 112);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "порог";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(184, 112);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(47, 13);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "T окна=";
+            // 
+            // for_T
+            // 
+            this.for_T.Location = new System.Drawing.Point(223, 109);
+            this.for_T.Name = "for_T";
+            this.for_T.Size = new System.Drawing.Size(46, 20);
+            this.for_T.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.for_N2_2);
+            this.Controls.Add(this.for_N1_2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox for_N2;
+        private System.Windows.Forms.TextBox for_v2;
+        private System.Windows.Forms.TextBox for_v3;
+        private System.Windows.Forms.TextBox for_N;
+        private System.Windows.Forms.TextBox for_fd;
+        private System.Windows.Forms.TextBox for_N1;
+        private System.Windows.Forms.TextBox for_v1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox for_N1_2;
+        private System.Windows.Forms.TextBox for_N2_2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox for_sill;
+        private System.Windows.Forms.TextBox for_T;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Zadanie2_IT/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie2_IT
+{
+    public partial class Form1 : Form
+    {
+        private double v1, v2, v3;
+        private double f_d;
+        private int N,N1,N2;
+        private int N12, N22;
+        private int padding = 10;
+        private int left_keys_padding = 20;
+        private PointF[] func_points;
+        private PointF[] diff;
+        private double[] pred;
+        private double t;
+        private int T;//количество отсчетов окна
+        private PointF[] oursill;//порог
+        private PointF[] convolut;//свертка
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            for_v1.Text = "3";
+            for_v2.Text = "13";
+            for_v3.Text = "6";
+            for_fd.Text = "150";
+            for_N.Text = "1000";
+            for_N1.Text = "200";
+            for_N2.Text = "600";
+            for_sill.Text = "0,05";
+            for_T.Text = "50";
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            pictureBox1.Update();
+            pictureBox2.Image = null;
+            pictureBox2.Update();
+            int wX1, hX1;
+            Graphics graphics1 = pictureBox1.CreateGraphics();
+            wX1 = pictureBox1.Width;
+            hX1 = pictureBox1.Height;
+            int wX2, hX2;
+            Graphics graphics2 = pictureBox2.CreateGraphics();
+            wX2 = pictureBox2.Width;
+            hX2 = pictureBox2.Height;
+            Pen pen = new Pen(Color.DarkRed, 2f);
+            Pen pen1 = new Pen(Color.Black, 2f);
+            Pen pen2 = new Pen(Color.Blue, 2f);
+            //Ось X
+            Point KX1, KX2;
+            KX1 = new Point(30, (hX1 / 2));
+            KX2 = new Point(wX1 - 10, (hX1 / 2));
+            graphics1.DrawLine(pen1, KX1, KX2);
+            Point KX11, KX21;
+            KX11 = new Point(30, (hX2 / 2));
+            KX21 = new Point(wX2- 10, (hX2 / 2));
+            graphics2.DrawLine(pen1, KX11, KX21);
+
+            //Ось Y
+            Point KY1, KY2;
+            KY1 = new Point(30, 10);
+            KY2 = new Point(30, hX1 - 10);
+            graphics1.DrawLine(pen1, KY1, KY2);
+            Point KY11, KY21;
+            KY11 = new Point(30, 10);
+            KY21 = new Point(30, hX2 - 10);
+            graphics2.DrawLine(pen1, KY11, KY21);
+            //сетка
+            int actual_width1 = wX1 - 2 * padding - left_keys_padding;
+            int actual_height1 = hX1 - 2 * padding;
+            int actual_width2 = wX2 - 2 * padding - left_keys_padding;
+            int actual_height2 = hX2 - 2 * padding;
+            int actual_top = padding;
+            int actual_bottom1 = actual_top + actual_height1;
+            int actual_left = padding + left_keys_padding;
+            int actual_right1 = actual_left + actual_width1;
+            int actual_bottom2 = actual_top + actual_height2;
+            int actual_right2 = actual_left + actual_width2;
+            Pen GridPen = new Pen(Color.Gray, 1f);
+            int grid_size = 11;
+            float maxY = 0;
+            GenerateSignal();
+            for (int i = 0; i < N; i++)
+            {
+                if (maxY < Math.Abs(func_points[i].Y)) maxY = Math.Abs(func_points[i].Y);//макс значение Y
+            }
+            PointF K1, K2, K3, K4;
+            PointF K5, K6, K7, K8;
+            for (double i = 0.5; i < grid_size; i += 1.0)
+            {
+                //вертикальная
+                K1 = new PointF((float)(actual_left + i * actual_width1 / grid_size), actual_top);
+                K2 = new PointF((float)(actual_left + i * actual_width1 / grid_size), actual_bottom1);
+                graphics1.DrawLine(GridPen, K1, K2);
+                K5 = new PointF((float)(actual_left + i * actual_width2 / grid_size), actual_top);
+                K6 = new PointF((float)(actual_left + i * actual_width2 / grid_size), actual_bottom2);
+                graphics2.DrawLine(GridPen, K5, K6);
+                double v = 0 + i * ((double)(N / f_d) - 0) / grid_size;
+                string s1 = v.ToString("0.00");
+                graphics1.DrawString(s1, new Font("Arial", 7), Brushes.Green, actual_left + (float)i * actual_width1 / grid_size, actual_bottom1 + 0);
+                graphics2.DrawString(s1, new Font("Arial", 7), Brushes.Green, actual_left + (float)i * actual_width2 / grid_size, actual_bottom2 + 0);
+                //горизонатальная
+                K3 = new PointF(actual_left, (float)(actual_top + i * actual_height1 / grid_size));
+                K4 = new PointF(actual_right1, (float)(actual_top + i * actual_height1 / grid_size));
+                K7 = new PointF(actual_left, (float)(actual_top + i * actual_height2 / grid_size));
+                K8 = new PointF(actual_right2, (float)(actual_top + i * actual_height2 / grid_size));
+                double g = 0 + i * (double)(maxY / grid_size);
+                string s2 = g.ToString("0.00");
+                graphics1.DrawString(s2, new Font("Arial", 7), Brushes.Green, actual_left - left_keys_padding, actual_bottom1 - (float)i * actual_height1 / grid_size - hX1 / 2);//????
+                graphics1.DrawLine(GridPen, K3, K4);
+                graphics2.DrawString(s2, new Font("Arial", 7), Brushes.Green, actual_left - left_keys_padding, actual_bottom2 - (float)i * actual_height2 / grid_size - hX2 / 2);//????
+                graphics2.DrawLine(GridPen, K7, K8);
+            }
+            PointF actual_tb = new PointF(actual_top, actual_bottom1);//для y
+            PointF actual_rl = new PointF(actual_right1, actual_left);//для x
+            PointF actual_tb2 = new PointF(actual_top, actual_bottom2);//для y
+            PointF actual_rl2 = new PointF(actual_right2, actual_left);//для x
+            PointF from_toX = new PointF(0, (float)(N / f_d));
+            PointF from_toY = new PointF(-maxY * (float)1.2, maxY * (float)1.2);
+            float maxY1=0;
+            Diffs();
+            LPFilter();
+            SearchBorder();
+            for_N1_2.Text = N12.ToString();
+            for_N2_2.Text = N22.ToString();
+            for (int i = 0; i < N; i++)
+            {
+                if (maxY1 < Math.Abs(diff[i].Y)) maxY1 = Math.Abs(diff[i].Y);//макс значение Y
+            }
+            PointF from_toY1 = new PointF(-maxY1 * (float)1.2, maxY1* (float)1.2);
+            convert_range_graph(ref func_points, actual_rl, actual_tb, from_toX, from_toY);
+            graphics1.DrawLines(pen1, func_points);
+            convert_range_graph(ref diff, actual_rl2, actual_tb2, from_toX, from_toY1);
+            convert_range_graph(ref oursill, actual_rl2, actual_tb2, from_toX, from_toY1);
+            convert_range_graph(ref  convolut, actual_rl2, actual_tb2, from_toX, from_toY1);
+            graphics2.DrawLines(pen1, diff);
+            graphics2.DrawLines(pen, oursill);
+            graphics2.DrawLines(pen2, convolut);
+        }
+        public void GenerateSignal()
+        {
+            if (for_N1.Text != "" || for_N2.Text != "")
+            {
+                N1 = Convert.ToInt32(for_N1.Text);
+                N2 = Convert.ToInt32(for_N2.Text);
+            }
+            else { MessageBox.Show("параметры N1,N2 по умолчанию", "Внимание!"); }
+            if (for_v1.Text != "" || for_v2.Text != "" || for_v3.Text != "")
+            {
+                v1 = Convert.ToDouble(for_v1.Text);
+                v2 = Convert.ToDouble(for_v2.Text);
+                v3 = Convert.ToDouble(for_v3.Text);
+            }
+            else { MessageBox.Show("параметры v по умолчанию", "Внимание!"); }
+            
+            if (for_fd.Text != "" || for_N.Text != "")
+            {
+                f_d = Convert.ToDouble(for_fd.Text);
+                N = Convert.ToInt32(for_N.Text);
+                T = Convert.ToInt32(for_T.Text);
+                t = Convert.ToDouble(for_sill.Text);
+
+            }
+            else { MessageBox.Show("параметры f_d,N,порога и окна по умолчанию", "Внимание!"); }
+            func_points = new PointF[N];
+            oursill = new PointF[N];
+            float dt = (float)(1 / f_d);
+            for (int i = 0; i < N1; i++) 
+            {
+                func_points[i]=new PointF((float)i * dt, (float)Math.Sin(2 * Math.PI * v1 * (float)i*dt + 0) );
+                oursill[i]= new PointF((float)i * dt, (float)t);//порог
+            }
+            double phi2 = 2 * Math.PI * N1 * (v1 - v2) / f_d;
+            for (int i = N1; i < N2; i++)
+            {
+                func_points[i] = new PointF((float)i * dt, (float)Math.Sin(2 * Math.PI * v2 * (float)i * dt + (float)phi2));
+                oursill[i] = new PointF((float)i * dt, (float)t);//порог
+            }
+            double phi3 = 2 * Math.PI * N2 * (v2 - v3) / f_d + phi2;
+            for (int i = N2; i < N; i++)
+            {
+                func_points[i] = new PointF((float)i * dt, (float)Math.Sin(2 * Math.PI * v3 * (float)i * dt + (float)phi3));
+                oursill[i] = new PointF((float)i * dt, (float)t);//порог
+            }
+        }
+        public void Diffs()
+        {
+            double w = 2 * Math.PI * v2;
+            double a1=(-2 * Math.Cos(w / f_d));
+            diff = new PointF[N];
+            pred = new double[N];
+            pred[0]= func_points[0].Y;
+            diff[0]= new PointF(func_points[0].X, (float)Math.Abs(func_points[0].Y - pred[0]));
+            pred[1] = (-func_points[1].Y * a1);
+            diff[1] = new PointF(func_points[1].X,(float)Math.Abs(func_points[1].Y - pred[1]));
+            for (int i = 2; i < N; i++)
+            {
+                double temp = -a1 * func_points[i-1].Y - func_points[i - 2].Y;
+                pred[i]=temp;
+                diff[i]= new PointF(func_points[i].X, (float)Math.Abs(pred[i] - func_points[i].Y));
+            }
+        }
+        public void LPFilter()
+        {
+            List<double> arr = new List<double>();
+            List<double> it = new List<double>();
+            double temp=0;
+            for (int i = (int)(T/ 2); i < (N - T / 2); i++)
+            {
+                for (int j = (int)(i - T / 2); j < (i + T/ 2); j++)
+                {
+                    temp += 1*diff[j].Y;
+                }
+                arr.Add(temp/T);
+                it.Add(func_points[i].X);
+                temp = 0;
+            }
+            convolut = new PointF[arr.Count];
+            for(int i=0;i<arr.Count;i++)
+            {
+                convolut[i] = new PointF((float)it[i],(float)arr[i] );
+            }
+        }
+        public void SearchBorder()
+        {
+            bool first=false;
+            for (int i = 0; i < N - T; i++)
+            {
+                if (convolut[i].Y <= t && first == false)//если меньше порога 
+                {
+                    first = true;
+                    N12 = i + T / 2;
+                }
+                if (convolut[i].Y <= t && first == true)
+                {
+                    N22 = i + T / 2;
+                }
+            }
+        }
+
+        public void convert_range_graph(ref PointF[] data, PointF actual_rl, PointF actual_tb, PointF from_toX, PointF from_toY)
+        {
+            //actual-размер:X-top/right Y-right,left
+            //from_to: X-мин, Y-макс
+            float kx = (actual_rl.X - actual_rl.Y) / (from_toX.Y - from_toX.X);
+            float ky = (actual_tb.X - actual_tb.Y) / (from_toY.Y - from_toY.X);
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i].X = (data[i].X - from_toX.X) * kx + actual_rl.Y;
+                data[i].Y = (data[i].Y - from_toY.X) * ky + actual_tb.Y;
+            }
+        }
+    }
+}
